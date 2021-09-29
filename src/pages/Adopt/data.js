fe1231c4e25f1d2de981846b68244f7e053877ff
Marcely Lopes dos Anjos <marcely.anjos@fcptn.local>
2021-09-29 18:06:50 -0300

    exemplificando adoção

---
+++ src/pages/Adopt/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    id: 1,
+    nome: 'nome1',
+    idade: '1 ano e 6 meses',
+    descrição: 'amigavel, gosta de ...',
+    imagem:
+      'https://scontent-for1-1.xx.fbcdn.net/v/t1.6435-9/124886216_1277380439297611_5112417500763159822_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeEVGX5D0E65qELcFmoq6PO09ewJimZpSR_17AmKZmlJH1e3Rzznv8AS_xfWpqoiVTG9lOSM7IwL5cbQEGq4BzHl&_nc_ohc=I7eEr-Xa1lEAX-w0kIx&_nc_ht=scontent-for1-1.xx&oh=f4a91e3b6de9b6fad3f3be39b5eca047&oe=617A92C8',
+  },
+  {
+    id: 2,
+    nome: 'nome2',
+    idade: '1 ano e 6 meses',
+    descrição: 'amigavel, gosta de ...',
+    imagem: 'https://scontent-for1-1.xx.fbcdn.net/v/t1.6435-9/123290555_1268426810192974_1915997393173606687_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeFg9Uk8BOmH9-1A5Qnbba8OySX0wsZrn-HJJfTCxmuf4TTZxv-muiZDuhIZ5RVWguCig76waBcvBMHKHW26h7v5&_nc_ohc=G9cNnkmq_XEAX97dB-p&tn=Lc9UxnXcFWcDsCr1&_nc_ht=scontent-for1-1.xx&oh=45035ad77813133e9eeb5592b39111e5&oe=6178AA13',
+  },
+  {
+    id: 3,
+    nome: 'nome2',
+    idade: '1 ano e 6 meses',
+    descrição: 'amigavel, gosta de ...',
+    imagem: 'https://scontent-for1-1.xx.fbcdn.net/v/t1.6435-9/122970660_1265389800496675_4059279334361494406_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeHDHVKx_tF7JFrz_sriRjPRoT5y-qEe0fyhPnL6oR7R_NUdard8e-4PkYrcA6uAnuuUJ8wEBGJFsZrnzb9KA_Me&_nc_ohc=4ZB5MHEhXzQAX8R4ZZC&_nc_ht=scontent-for1-1.xx&oh=cc2a6c6204cd0b0f758560119a3fa911&oe=617B0B75',
+  },
+];
+
+export {data};
--- src/pages/Adopt/index.js
+++ src/pages/Adopt/index.js
@@ -2,39 +2,70 @@ import React, {useRef, useCallback} from 'react';
 import {
   Image,
   ImageBackground,
-//   Button,
+  //   Button,
   SafeAreaView,
+  FlatList,
   StyleSheet,
   Text,
 } from 'react-native';
-import { Button } from 'react-native-elements'
+import {Button} from 'react-native-elements';
+import {ScrollView} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {data} from './data';
 
 export default function Home() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor: '#EFF9FF', height: '100%'}}>
       <SafeAreaView
         style={{
-          height: '23%',
+          height: '7%',
           backgroundColor: '#ffffff',
           display: 'flex',
+          flexDirection: 'row',
           justifyContent: 'center',
           alignItems: 'center',
         }}>
-        <SafeAreaView style={{width: '95%', display: 'flex'}}>
+        <SafeAreaView style={{width: '85%', justifyContent: 'space-between'}}>
           <Text>Adoção</Text>
+        </SafeAreaView>
+        <SafeAreaView>
           <Button
-          icon={
-            <Icon
-            name="filter"
-            size={15}
-            color="black"
-          />
-          }
+            type="clear"
+            // buttonStyle={{ width:'10%'}}
+            icon={<Icon name="filter" size={20} color="black" />}
             // onPress={() => Alert.alert('Simple Button pressed')}
           />
         </SafeAreaView>
       </SafeAreaView>
+      
+        {/* <ScrollView> */}
+          <FlatList
+            data={data}
+            keyExtractor={item => item.id}
+            numColumns={2}
+            renderItem={({item}) => {
+              return (
+                <SafeAreaView style={{ width:'50%', alignItems:'center',display:'flex', justifyContent:'center'}}>
+                  <SafeAreaView
+                  style={{
+                    alignItems: 'center',
+                    backgroundColor: '#ffffff',
+                    // flexGrow: 1,
+                    width:146,
+                    height:211.98,
+                    margin: 20,
+                    // padding: 20,
+                    borderRadius:12.34
+                  }}>
+                  <Image style={{width:146, height:124.3, borderTopLeftRadius:12.34, borderTopRightRadius:12.34}} source={{uri:item.imagem}} />
+                  <Text style={{color: '#333333'}}>{item.nome}</Text>
+                </SafeAreaView>
+                </SafeAreaView>
+              );
+            }}
+          />
+          {/* </ScrollView> */}
+      
     </SafeAreaView>
   );
 }
